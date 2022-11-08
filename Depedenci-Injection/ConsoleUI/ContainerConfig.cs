70da7b6e86f5bd90a9fdf61d0597691fec7670e8
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-08 17:25:36 +0700

    [Dependency-Injection] Implement other class for IBusinessLogic

--- Depedenci-Injection/ConsoleUI/ContainerConfig.cs
+++ Depedenci-Injection/ConsoleUI/ContainerConfig.cs
@@ -15,7 +15,7 @@ namespace ConsoleUI
         {
             var builder= new ContainerBuilder();
             builder.RegisterType<Application>().As<IApplication>();
-            builder.RegisterType<BusinessLogic>().As<IBusinessLogic>();
+            builder.RegisterType<BetterBusinessLogic>().As<IBusinessLogic>();
             builder.RegisterAssemblyTypes(Assembly.Load(nameof(DemoLibrary)))
                 .Where(t=>t.Namespace.Contains("Utilities"))
                 .As(t=>t.GetInterfaces().FirstOrDefault(i=>i.Name=="I"+t.Name));
