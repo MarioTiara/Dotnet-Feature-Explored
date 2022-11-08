c65d697640cf8a452a82bc8e05eb27dcce1239c9
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-08 13:54:07 +0700

    [Dependency-Injection] Add Application

--- Depedenci-Injection/ConsoleUI/ContainerConfig.cs
+++ Depedenci-Injection/ConsoleUI/ContainerConfig.cs
@@ -14,11 +14,13 @@ namespace ConsoleUI
         public static IContainer Configure()
         {
             var builder= new ContainerBuilder();
+            builder.RegisterType<Application>().As<IApplication>();
             builder.RegisterType<BusinessLogic>().As<IBusinessLogic>();
-
             builder.RegisterAssemblyTypes(Assembly.Load(nameof(DemoLibrary)))
                 .Where(t=>t.Namespace.Contains("Utilities"))
                 .As(t=>t.GetInterfaces().FirstOrDefault(i=>i.Name=="I"+t.Name));
+
+
             return builder.Build();
         }
 
