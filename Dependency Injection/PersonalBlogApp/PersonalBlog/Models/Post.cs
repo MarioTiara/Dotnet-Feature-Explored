95a0a7f3163b013b975f05186a3c848872309811
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-10 05:07:11 +0700

    [Dependency Injection][PersonalBlog] Add Post Class

---
+++ Dependency Injection/PersonalBlogApp/PersonalBlog/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PersonalBlog.Models
+{
+
+    [Table("post")]
+    public class Post
+    {
+        [Key]
+        public string Id { get; set; }
+        public DateTime PostDatatime { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
