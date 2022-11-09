1d3acf65e96ced82727f37eb81c3eefa3ce4cf79
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-10 05:21:15 +0700

    [Dependency Injection][PersonalBlog] Add DbContext class

---
+++ Dependency Injection/PersonalBlogApp/PersonalBlog/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace PersonalBlog.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Post> posts { get; set; }
+    }
+}
