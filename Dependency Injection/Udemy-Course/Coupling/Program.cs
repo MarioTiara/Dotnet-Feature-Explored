dbf5d7e50ae97ca279c741c4600e98899607eaf0
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-09 11:27:34 +0700

    [Dependency-Injection] Class coupling

--- Dependency Injection/Udemy-Course/Coupling/Program.cs
+++ Dependency Injection/Udemy-Course/Coupling/Program.cs
@@ -5,3 +5,33 @@
         Console.WriteLine("Hello, World!");
     }
 }
+
+public class UserInterface
+{
+    public void GetData(){
+        Console.WriteLine("Enter your username:");
+        var userName=Console.ReadLine();
+
+        Console.Write("Enter your password");
+        var password = Console.ReadLine();
+
+        var business= new Business();
+        business.SignUp(userName, password);
+
+    }
+}
+
+public class Business
+{
+    public void SignUp(string userName, string password){
+        var dataAccess= new DataAccess();
+        dataAccess.Store(userName, password);
+    }
+}
+
+public class DataAccess
+{
+    public void Store(string userName, string password){
+
+    }
+}
