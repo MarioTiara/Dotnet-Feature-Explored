26ab53218b0d29f29b3c9b85bfbda05934cf02eb
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-11 07:53:40 +0700

    [Entityframework] Add models

---
+++ EntityFranework/EFDemoApp/EFDataAccessLibrary/Models/Email.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDataAccessLibrary.Models
+{
+    internal class EmailAddress
+    {
+    }
+}
---
+++ EntityFranework/EFDemoApp/EFDataAccessLibrary/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDataAccessLibrary.Models
+{
+    public class Person
+    {
+        int Id { get; set; }    
+        public string FirstName { get; set; }
+        public int LastName { get; set; }
+        public int Age { get; set; }
+        public List<Address> Addresses { get; set; }
+    }
+}
