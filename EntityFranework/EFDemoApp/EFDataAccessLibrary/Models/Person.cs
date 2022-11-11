158de9abfe98e5dbc582f6d8759be2294e291e96
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-11 21:08:43 +0700

    [EF] Added Validation and modify memory range

--- EntityFranework/EFDemoApp/EFDataAccessLibrary/Models/Person.cs
+++ EntityFranework/EFDemoApp/EFDataAccessLibrary/Models/Person.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace EFDataAccessLibrary.Models
 {
     public class Person
     {
-        int Id { get; set; }    
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
         public int LastName { get; set; }
+        [Required]
         public int Age { get; set; }
-        public List<Address> Addresses { get; set; }
+        public List<Address> Addresses { get; set; }= new List<Address>();
+        public List<Email> Emails { get; set; }= new List<Email>(); 
     }
 }
