0a1114f750d5ca815b71d9322177f8f91b97230f
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-12 19:36:38 +0700

    [EF] change (int LastName) =>  (string LastName)

--- EntityFranework/EFDemoApp/EFDataAccessLibrary/Models/Person.cs
+++ EntityFranework/EFDemoApp/EFDataAccessLibrary/Models/Person.cs
@@ -7,17 +7,17 @@ namespace EFDataAccessLibrary.Models
 {
     public class Person
     {
-        [Key]
+        
         public int Id { get; set; }
         [Required]
         [MaxLength(50)]
         public string FirstName { get; set; }
         [Required]
         [MaxLength(50)]
-        public int LastName { get; set; }
+        public string LastName { get; set; }
         [Required]
         public int Age { get; set; }
         public List<Address> Addresses { get; set; }= new List<Address>();
-        public List<Email> Emails { get; set; }= new List<Email>(); 
+        public List<Email> EmailAddresses { get; set; }= new List<Email>(); 
     }
 }
