2556abf716376c46dfab7ed7f59840e99fe4e18e
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-13 10:40:37 +0700

    [Acync] Add SummonDogLucally

--- Async/Program.cs
+++ Async/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-
+using System.IO;
+using System.Threading.Tasks;
 namespace Async
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SummonDogLocally();
         }
+
+        static async Task SummonDogLocally(){
+            Console.WriteLine("1. Summoning Dog Locally ...");
+            
+            //read all the text inside the dog.text async
+            string dogtext=await File.ReadAllTextAsync("dog.txt");
+            
+            //display the data inside the txt file
+            Console.WriteLine($"2. Dog Summoned Locally \n{dogtext}");
+
+        }
+       
     }
+
+
 }
