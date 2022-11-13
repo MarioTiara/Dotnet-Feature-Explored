0bda7824617eb73ff42de3883bb67660ae4647b2
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-13 11:12:03 +0700

    [Acync] Add SummonDog class

--- Async/Program.cs
+++ Async/Program.cs
@@ -11,27 +11,8 @@ namespace Async
         static async Task Main(string[] args)
         {
             string URL= "https://raw.githubusercontent.com/l3oxer/Doggo/main/README.md";
-            var taks= new List<Task> {SummonDogLocally(), SummonDogFromURL(URL)};
+            var taks= new List<Task> {SummonDog.SummonDogLocally(), SummonDog.SummonDogFromURL(URL)};
             await Task.WhenAll(taks);
-        }
-
-        static async Task SummonDogLocally(){
-            Console.WriteLine("1. Summoning Dog Locally ...");
-            
-            //read all the text inside the dog.text async
-            string dogtext=await File.ReadAllTextAsync("dog.txt");
-            
-            //display the data inside the txt file
-            Console.WriteLine($"2. Dog Summoned Locally \n{dogtext}");
-
-        }
-
-        static async Task SummonDogFromURL (string URL){
-            Console.WriteLine("1. Summoning Dog from URL ...");
-            using (var httpclient= new HttpClient()){
-                string result = await httpclient.GetStringAsync(URL);
-                  Console.WriteLine($"2. Dog Summoned url \n{result}");
-            }
         }
        
     }
