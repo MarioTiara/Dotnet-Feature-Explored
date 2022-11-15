b381ebf9ff967106ab7b8eb838d5c17ac9a77e8e
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-15 14:41:49 +0700

    [MinimalAPI] add  DataAccess/Data

---
+++ MinimalAPIDemoApp/DataAccess/Data/UserData.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.DbAccess;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    public class UserData
+    {
+        private readonly ISqlDataAccess _db;
+        public UserData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+        public Task<IEnumerable<UserModel>> GetUsers() =>
+            _db.LoadData<UserModel, dynamic>("dbo.spUser_GetAll", new { });
+
+        public async Task<UserModel?> GetUser(int id)
+        {
+            var result = await _db.LoadData<UserModel, dynamic>(
+                "dbo.spUser_Get",
+                new { Id = id });
+            return result.FirstOrDefault();
+        }
+    }
+}
