df1bc48820bcf526c459c385197a669ecc2e3eee
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-15 22:48:35 +0700

    [MinimalAPI] Final Project

--- MinimalAPIDemoApp/DataAccess/Data/UserData.cs
+++ MinimalAPIDemoApp/DataAccess/Data/UserData.cs
@@ -1,14 +1,10 @@
 ﻿using DataAccess.DbAccess;
 using DataAccess.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccess.Data
 {
-    public class UserData
+    public class UserData : IUserData
     {
         private readonly ISqlDataAccess _db;
         public UserData(ISqlDataAccess db)
@@ -25,5 +21,15 @@ namespace DataAccess.Data
                 new { Id = id });
             return result.FirstOrDefault();
         }
+
+        public Task InsertUser(UserModel user) =>
+            _db.SaveData("dbo.spUser_Insert", new { user.FirstName, user.LastName });
+
+        public Task UpdateUser(UserModel user) =>
+            _db.SaveData("dbo.spUser_Update", user);
+
+        public Task DeleteUser(int id) =>
+            _db.SaveData("dbo.spUser_Delete", new { Id = id });
+
     }
 }
