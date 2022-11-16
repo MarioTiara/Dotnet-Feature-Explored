f1014faffb83d39e1f1cfd7d3465b87ff94db8aa
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-16 20:08:53 +0700

    [JWT] Inject JWTConfig class

--- MinimalAPIDemoApp/MinimalAPIDemo/Program.cs
+++ MinimalAPIDemoApp/MinimalAPIDemo/Program.cs
@@ -1,5 +1,6 @@
 using DataAccess.DbAccess;
 using MinimalAPIDemo;
+using MinimalAPIDemo.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
 builder.Services.AddSingleton<IUserData, UserData>();
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
 
 var app = builder.Build();
 
