7783a5845a87e6d5f9c507bcc32d531b42d3726e
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-17 20:21:33 +0700

    [MinimalAPI] Add Role in UserModel

--- MinimalAPIDemoApp/DataAccess/Models/UserModel.cs
+++ MinimalAPIDemoApp/DataAccess/Models/UserModel.cs
@@ -12,6 +12,7 @@ public class UserModel
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string EmailAddress { get; set; }
-    public string Password { get; set; }    
+    public string Password { get; set; }
+    public string Role { get; set; }
 
 }
