0b10d0e6d2f3df660c28c9c8ef2847328886ba1f
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-18 08:23:10 +0700

    [JWT] Modify JwtConfig

--- MinimalAPIDemoApp/MinimalAPIDemo/Configuration/JwtConfig.cs
+++ MinimalAPIDemoApp/MinimalAPIDemo/Configuration/JwtConfig.cs
@@ -4,6 +4,8 @@ namespace MinimalAPIDemo.Configuration
 {
     public class JwtConfig
     {
-        public string Secret {get;set;}
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
     }
 }
