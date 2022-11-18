47174a17f8edeb728d3694319c394e9244bac2c1
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-18 08:26:54 +0700

    [MinimalAPI] Add spUser_GetbyUserName

---
+++ MinimalAPIDemoApp/UserDB/dbo/StoredProcedures/User/spUser_GetbyUserName.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spUser_GetbyUserName]
+	@Username varchar(50)
+AS
+begin
+	select Id, [UserName],FirstName, LastName,EmailAddress, Password, Role
+	from dbo.[User]
+	where [UserName]=@Username
+end
