ae5832b7510fcc82c5e193993c5e96b4a2281b79
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-18 08:26:09 +0700

    [JWT] Add Login endpoint

--- MinimalAPIDemoApp/MinimalAPIDemo/Api.cs
+++ MinimalAPIDemoApp/MinimalAPIDemo/Api.cs
@@ -1,4 +1,8 @@
-﻿namespace MinimalAPIDemo;
+﻿using MinimalAPIDemo.Services;
+using MinimalAPIDemo.Models;
+
+
+namespace MinimalAPIDemo;
 
 public static class Api
 {
@@ -6,10 +10,21 @@ public static class Api
     {
         // All of my API endpoint mapping
         app.MapGet("/Users", GetUsers);
-        app.MapGet("/Users/{id}", GetUser);
+       // app.MapGet("/Users/{id}", GetUserById);
+        app.MapGet("/Users/{UserName}", GetUserByUserName);
         app.MapPost("/Users", InsertUser);
         app.MapPut("/Users", UpdateUser);
         app.MapDelete("/Users", DeleteUser);
+    }
+
+    private static async Task<IResult> Login(ModelUserLogin user, IUserData data)
+    {
+        if (!string.IsNullOrEmpty(user.UserName)&& !string.IsNullOrEmpty(user.Password)){
+            var loggedUser=await data.GetUser(user.UserName);
+            if (loggedUser is null) return Results.NotFound("User not found");
+            if (loggedUser.Password != user.Password) return Results.NotFound("UserName and Password is not Mattching");
+            string jwttoken=Auth
+        }
     }
 
     private static async Task<IResult> GetUsers(IUserData data)
@@ -24,11 +39,25 @@ public static class Api
         }
     }
 
-    private static async Task<IResult> GetUser(int id, IUserData data)
+    private static async Task<IResult> GetUserById(int id, IUserData data)
     {
         try
         {
             var results = await data.GetUser(id);
+            if (results == null) return Results.NotFound();
+            return Results.Ok(results);
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> GetUserByUserName(string UserName, IUserData data)
+    {
+        try
+        {
+            var results = await data.GetUser(UserName);
             if (results == null) return Results.NotFound();
             return Results.Ok(results);
         }
