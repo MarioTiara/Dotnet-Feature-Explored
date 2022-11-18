cd0d19c434807bb5a99217162bd2b1fb19e0515a
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-18 08:21:19 +0700

    [MinimalAPI] Modify User Table

--- MinimalAPIDemoApp/UserDB/dbo/Tables/User.sql
+++ MinimalAPIDemoApp/UserDB/dbo/Tables/User.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [dbo].[User]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[UserName] NVARCHAR(50) NOT NULL,
     [FirstName] NVARCHAR(50) NOT NULL, 
     [LastName] NVARCHAR(50) NOT NULL, 
     [EmailAddress] NVARCHAR(50) NOT NULL, 
     [Password] NVARCHAR(10) NOT NULL, 
-    [Role] NVARCHAR(10) NOT NULL
+    [Role] NVARCHAR(50) NOT NULL,
 )
