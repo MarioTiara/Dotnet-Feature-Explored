3fe55c9559a2425f0f8c6214b5d700adbf079b58
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-19 21:22:04 +0700

    [JWT] Add MovieEndpoint with Authorization

--- MinimalAPIDemoApp/MinimalAPIDemo/Endpoint/MovieEndPoint.cs
+++ MinimalAPIDemoApp/MinimalAPIDemo/Endpoint/MovieEndPoint.cs
@@ -1,13 +1,62 @@
-﻿namespace MinimalAPIDemo.Endpoint
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MinimalAPIDemo.Endpoint
 {
     public class MovieEndPoint
     {
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator, Standard")]
         public async Task<IResult> GetMovies(IMovieData data)
         {
             try
             {
 
                 return Results.Ok(await data.GetMovies());
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
+        public async Task<IResult> CreateMovie(MovieModel movie, IMovieData data)
+        {
+            try
+            {
+                await data.InsertMovie(movie);
+                return Results.Ok("Movie Created");
+                
+            }catch(Exception ex)
+            {
+               return Results.Problem(ex.Message);
+            }
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
+        public async Task<IResult> UpdateMovie(MovieModel movie, IMovieData data)
+        {
+            try
+            {
+                await data.UpdateMovie(movie);
+                return Results.Ok("Movie Update");
+
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
+        public async Task<IResult> DeleteMovie(int id, IMovieData data)
+        {
+            try
+            {
+                await data.DeleteMovie(id);
+                return Results.Ok($"Movie with id: {id} was deleted");
+
             }
             catch (Exception ex)
             {
