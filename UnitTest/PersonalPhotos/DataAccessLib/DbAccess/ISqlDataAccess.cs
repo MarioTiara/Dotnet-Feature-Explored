4f892b20e263f3173dae9d631010ddb2c4b7ccde
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-22 19:54:39 +0700

    [UnitTest/PersonalPhotos] Add DataAccess SqlDataAccess

---
+++ UnitTest/PersonalPhotos/DataAccessLib/DbAccess/ISqlDataAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace DataAccessLib.DbAccess
+{
+    public interface ISqlDataAccess
+    {
+        Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U paramaters, string connectionId = "Default");
+        Task SaveData<T>(string storedProcedures, T paramaters, string connectionId = "Default");
+    }
+}
