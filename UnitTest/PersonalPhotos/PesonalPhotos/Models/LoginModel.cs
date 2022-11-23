c7310ef9ec0befe15457c1b1c99966d0ea4b5877
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-23 21:25:58 +0700

    [UnitTest/PersonalPhotos] Add LoginModel

---
+++ UnitTest/PersonalPhotos/PesonalPhotos/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PesonalPhotos.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage ="Please provide the email address")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Please provide password")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
