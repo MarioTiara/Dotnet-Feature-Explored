9059865098d4e72e8744ace3944879abf9e2a206
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-23 08:19:30 +0700

    [UnitTest/PersonalPhotos] Add PhotosData

---
+++ UnitTest/PersonalPhotos/DataAccessLib/Data/IPhotosData.cs
@@ -0,0 +1,11 @@
+﻿using DataAccessLib.Models;
+
+namespace DataAccessLib.Data
+{
+    public interface IPhotosData
+    {
+        Task<PhotosModel?> GetPhoto(int id);
+        Task<IEnumerable<PhotosModel>> GetPhotos();
+        Task InsertPhoto(PhotosModel photo, string email);
+    }
+}
---
+++ UnitTest/PersonalPhotos/DataAccessLib/Data/PhotosData.cs
@@ -0,0 +1,39 @@
+﻿using DataAccessLib.DbAccess;
+using DataAccessLib.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLib.Data
+{
+    public class PhotosData : IPhotosData
+    {
+        private readonly ISqlDataAccess _db;
+        public PhotosData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<PhotosModel>> GetPhotos()
+        {
+            return await _db.LoadData<PhotosModel, dynamic>("dbo.", new { });
+        }
+
+        public async Task<PhotosModel?> GetPhoto(int id)
+        {
+            return (await _db.LoadData<PhotosModel, dynamic>("dbo.spPhotos_Get", new { Id = id })).FirstOrDefault();
+        }
+
+        public Task InsertPhoto(PhotosModel photo, string email) =>
+            _db.SaveData("dbo.spPhotos_Insert", new
+            {
+                Email = email,
+                Description = photo.Description,
+                FileName = photo.FileName
+            });
+
+    }
+}
