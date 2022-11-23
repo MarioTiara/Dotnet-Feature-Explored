996412dca983bc149c0fb05e3724c500e288c762
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-23 07:51:20 +0700

    [UnitTest/PersonalPhotos] Add DataAccess/Data

---
+++ UnitTest/PersonalPhotos/DataAccessLib/Data/IUserData.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLib.Models;
+
+namespace DataAccessLib.Data
+{
+    public interface IUserData
+    {
+        Task<UsersModel?> GetUser(int id);
+        Task<IEnumerable<UsersModel>> GetUsers();
+        Task InsertMovie(UsersModel user);
+        Task UpdateUser(int id);
+    }
+}
---
+++ UnitTest/PersonalPhotos/DataAccessLib/Data/UserData.cs
@@ -0,0 +1,36 @@
+﻿using DataAccessLib.DbAccess;
+using DataAccessLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLib.Data
+{
+    public class UserData : IUserData
+    {
+        private readonly ISqlDataAccess _db;
+        public UserData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<UsersModel>> GetUsers()
+        {
+            return await _db.LoadData<UsersModel, dynamic>("dbo.spUser_GetAll", new { });
+        }
+
+        public async Task<UsersModel?> GetUser(int id)
+        {
+            var result = await _db.LoadData<UsersModel, dynamic>("dbo.spUser_Get", new { Id = id });
+            return result.FirstOrDefault();
+        }
+
+        public Task InsertMovie(UsersModel user) =>
+            _db.SaveData("dbo.spUser_Insert", user);
+
+        public Task UpdateUser(int id) =>
+            _db.SaveData("dbo.spUser_Update", new { Id = id });
+    }
+}
