5fee6fad0457c1251f67bae98c186faf76e8e38a
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-23 21:26:19 +0700

    [UnitTest/PersonalPhotos] Add PhotoUploadViewModel

---
+++ UnitTest/PersonalPhotos/PesonalPhotos/Models/PhotoUploadViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PesonalPhotos.Models
+{
+    public class PhotoUploadViewModel
+    {
+        [Required(ErrorMessage ="Please enter a description")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please choose a photo!")]
+        public IFormFile file { get; set; }
+    }
+}
