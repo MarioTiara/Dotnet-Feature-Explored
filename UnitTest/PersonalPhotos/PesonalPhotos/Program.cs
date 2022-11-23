f00414e143ae746215e1dc685d3c13708c6d6a79
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-23 10:43:29 +0700

    [UnitTest] Upgrade to MVC project

---
+++ UnitTest/PersonalPhotos/PesonalPhotos/Program.cs
@@ -0,0 +1,34 @@
+using DataAccessLib.Data;
+using DataAccessLib.DbAccess;
+using DataAccessLib.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+builder.Services.AddSingleton<IUserData, UserData>();
+builder.Services.AddSingleton<IPhotosData, PhotosData>();
+builder.Services.AddSingleton<ILocalFileStorage, LocalFileStorage>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
