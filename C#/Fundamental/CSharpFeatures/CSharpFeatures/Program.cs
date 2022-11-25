8ea17c36080a589bf369d1f0aec061934719cafd
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-25 16:54:28 +0700

    [C#] Records

---
+++ C#/Fundamental/CSharpFeatures/CSharpFeatures/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpFeatures
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+             Records.Person person = new("Nancy", "Davolio");
+             Console.WriteLine(person);
+        }
+    }
+}
---
+++ C#/Fundamental/CSharpFeatures/CSharpFeatures/Records.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFeatures
+{
+    public static class Records
+    {
+        public record Person(string name, string latsname);
+    }
+}
