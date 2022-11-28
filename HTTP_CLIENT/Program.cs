681d97286043df151b063da7dbfcbadf9c7b14c5
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-28 23:37:12 +0700

    Using HttpResponseMessage

--- HTTP_CLIENT/Program.cs
+++ HTTP_CLIENT/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static HttpClient client= new HttpClient();
+    private static async Task Main(string[] args)
+    {
+     try{
+        HttpResponseMessage responsemessage= await client.GetAsync("https://jsonplaceholder.typicode.com/users");
+        if(responsemessage.IsSuccessStatusCode){
+            string responseBody= await responsemessage.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+        }else{
+            Console.WriteLine(responsemessage.StatusCode);
+        }
+
+     }catch(HttpRequestException e){
+        Console.WriteLine($"Exception Caught! Message{0}", e.Message);
+     }
+    }
+}
